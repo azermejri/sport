d1350571a73a284df5ed989d4087a4fffbefca57
Azer Mejri <azermejri49@gmail.com>
2024-06-01 13:33:26 +0100

    version à déployer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CupEventComponent } from './components/cup-event/cup-event.component';
+import { ResultComponent } from './components/result/result.component';
+import { NewsComponent } from './components/news/news.component';
+import { StandingsComponent } from './components/standings/standings.component';
+import { BlogComponent } from './components/blog/blog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    CupEventComponent,
+    ResultComponent,
+    NewsComponent,
+    StandingsComponent,
+    BlogComponent,
+ 
   ],
   imports: [
     BrowserModule,
